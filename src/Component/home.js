a5b75ebc579cab86d2115cd1ffc23cb27321ab3e
Rajat <rajatdhoot123@gmail.com>
2019-06-01 15:00:57 +0530

    Local storage functionality implemented

--- src/Component/home.js
+++ src/Component/home.js
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 import DropZoneS from "./dropzone";
 import { Table, FormControl } from "react-bootstrap";
 
-const heading = {heading: JSON.stringify(["Rank","Name","Platform","Year","Genre","Publisher","Global_Sales",""])}
+const heading = {
+  heading: JSON.stringify([
+    "Rank",
+    "Name",
+    "Platform",
+    "Year",
+    "Genre",
+    "Publisher",
+    "Global_Sales",
+    ""
+  ])
+};
 
 const formatData = data => {
   let formattedArray = [];
@@ -19,16 +30,34 @@ const formatData = data => {
 };
 
 const getSearchList = (text, data) => {
-    let regex = new RegExp(text)
-    return data.filter(game => Object.keys(game)[0].toLocaleUpperCase().match(regex))
-}
+  let regex = new RegExp(text);
+  return data.filter(game =>
+    Object.keys(game)[0]
+      .toLocaleUpperCase()
+      .match(regex)
+  );
+};
 
 export default class Home extends Component {
   state = {
-    data: [],
+    data: [heading],
     searchList: [heading],
     searchText: ""
   };
+
+  componentDidMount() {
+    this.setState({
+      data: [
+        heading,
+        ...Object.keys(localStorage)
+          .filter(key => key.match(/^GAME/))
+          .map(game => {
+            let parseGame = JSON.parse(localStorage[game]);
+            return { [`GAME-${parseGame[1]}`]: localStorage[game] };
+          })
+      ]
+    });
+  }
 
   handleData = (data, cb) => {
     this.setState({
@@ -38,24 +67,28 @@ export default class Home extends Component {
 
   handleChange = e => {
     let search = e.target.value;
-    console.log(getSearchList(search, this.state.data))
     this.setState(
       {
         searchText: search
       },
       () => {
         this.setState({
-          searchList: [heading, ...getSearchList(search.toLocaleUpperCase(), this.state.data)]
+          searchList: [
+            heading,
+            ...getSearchList(search.toLocaleUpperCase(), this.state.data)
+          ]
         });
       }
     );
   };
 
   render() {
-    const [first = {}, ...rest] = this.state.searchList.length > 1 ? this.state.searchList : this.state.data;
-    console.log(first)
-    let heading = [];
+    const [first = {}, ...rest] =
+      this.state.searchList.length > 1
+        ? this.state.searchList
+        : this.state.data;
     console.warn({ state: this.state });
+    let heading = [];
     if (Object.keys(first).length) {
       heading = JSON.parse(first.heading);
     }
@@ -82,7 +115,7 @@ export default class Home extends Component {
           </thead>
           <tbody>
             {rest.map(games => {
-                let game = JSON.parse(Object.values(games)[0])
+              let game = JSON.parse(Object.values(games)[0]);
               return (
                 <tr key={game[0]}>
                   <td>{game[0]}</td>
